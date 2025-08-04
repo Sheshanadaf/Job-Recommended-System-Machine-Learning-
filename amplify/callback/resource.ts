a24b00740c2519c08e07965175cb960b4a5fa0d9
sheshanadaf <sheshanhebron539@gmail.com>
2025-08-04 19:57:28 +0530

    fresh

--- amplify/callback/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const callbackFunction = defineFunction({
-  name: 'callback-function',
-  entry: './handler.ts',
-});
--- amplify/user/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const userFunction = defineFunction({
-  name: 'user-function',
-  entry: './handler.ts',
-});
